04e9ec54fe66107810e6af142979d107d60ec3e4
bluegecko <kei09doya@gmail.com>
2025-01-31 14:25:48 +0900

    Added dc subcommand and open command to ArgumentsParser

--- src/main/kotlin/me/bluegecko/framelessviewer/ArgumentsParser.kt
+++ src/main/kotlin/me/bluegecko/framelessviewer/ArgumentsParser.kt
@@ -1,15 +1,33 @@
 package me.bluegecko.framelessviewer
 
-import picocli.CommandLine.Command
-import picocli.CommandLine.Option
+import picocli.CommandLine.*
 
-@Command(name = "FramelessViewer", mixinStandardHelpOptions = true)
+@Command(name = "FramelessViewer", mixinStandardHelpOptions = true, subcommands = [DCSubcomand::class])
 class ArgumentsParser : Runnable {
     @Option(names = ["-d", "--daemon"], description = ["Start with daemon"])
     var daemon = false
 
     @Option(names = ["-p", "--path"], description = ["Path of the image to be loaded from the beginning"])
     var initPath = ""
+
+    override fun run() {
+    }
+}
+
+@Command(
+    name = "dc",
+    mixinStandardHelpOptions = true,
+    description = ["Daemon Client Subcommands (Daemon mode must be enabled)"],
+    subcommands = [DCSubcomand.OpenCommand::class]
+)
+class DCSubcomand : Runnable {
+    @Command(name = "open", description = ["Open a file"])
+    class OpenCommand : Runnable {
+        @Parameters(index = "0", description = ["Path of the file"], paramLabel = "PATH")
+        var path = ""
+
+        override fun run() {}
+    }
 
     override fun run() {}
 }
