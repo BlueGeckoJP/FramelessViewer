fccb255f49b52fd8b230ae5febb9a8414a479673
bluegecko <kei09doya@gmail.com>
2025-04-16 11:21:46 +0900

    Logger implementation changed to logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.17")
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
-    implementation("org.slf4j:slf4j-simple:2.0.17")
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.5.18")
 }
 
 tasks.test {
--- src/main/kotlin/me/bluegecko/framelessviewer/Main.kt
+++ src/main/kotlin/me/bluegecko/framelessviewer/Main.kt
@@ -1,9 +1,17 @@
 package me.bluegecko.framelessviewer
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
 import com.formdev.flatlaf.themes.FlatMacDarkLaf
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import picocli.CommandLine
+import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.UIManager
 
@@ -11,6 +19,8 @@ lateinit var appController: AppController
 var daemon: Daemon? = null
 
 fun main(args: Array<String>) = runBlocking {
+    setupLogger()
+
     val logger = LoggerFactory.getLogger(this::class.java)
 
     UIManager.setLookAndFeel(FlatMacDarkLaf())
@@ -37,4 +47,42 @@ fun main(args: Array<String>) = runBlocking {
     }
 
     appController.run(argumentsParser.initPath)
+}
+
+fun setupLogger() {
+    val saveDir = File(System.getProperty("user.home"), "/.framelessviewer")
+    if (!saveDir.exists()) {
+        saveDir.mkdir()
+    }
+
+    val logFile = saveDir.resolve("latest.log")
+    if (logFile.exists()) {
+        logFile.delete()
+    }
+
+    val context = LoggerFactory.getILoggerFactory() as LoggerContext
+
+    val encoder = PatternLayoutEncoder()
+    encoder.context = context
+    encoder.pattern = "[%d{HH:mm:ss.SSS}] [%thread] %-5level %logger{36} - %msg%n"
+    encoder.start()
+
+    val fileAppender = FileAppender<ILoggingEvent>()
+    fileAppender.context = context
+    fileAppender.name = "FILE"
+    fileAppender.file = saveDir.resolve("latest.log").path
+    fileAppender.encoder = encoder
+    fileAppender.start()
+
+    val consoleAppender = ConsoleAppender<ILoggingEvent>()
+    consoleAppender.context = context
+    consoleAppender.name = "CONSOLE"
+    consoleAppender.encoder = encoder
+    consoleAppender.start()
+
+    val rootLogger = context.getLogger(Logger.ROOT_LOGGER_NAME)
+    rootLogger.detachAndStopAllAppenders()
+    rootLogger.addAppender(fileAppender)
+    rootLogger.addAppender(consoleAppender)
+    rootLogger.level = Level.DEBUG
 }
