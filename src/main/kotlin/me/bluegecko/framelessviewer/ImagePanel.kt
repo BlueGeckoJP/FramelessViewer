199566c0661d663d257aa58a1eb7a5c6b1e91b81
bluegecko <kei09doya@gmail.com>
2025-04-16 13:14:03 +0900

    removed fileList log

--- src/main/kotlin/me/bluegecko/framelessviewer/ImagePanel.kt
+++ src/main/kotlin/me/bluegecko/framelessviewer/ImagePanel.kt
@@ -181,8 +181,6 @@ class ImagePanel(val app: App, data: ImagePanelData) : JPanel() {
                         return@Comparator -1
                     })
         }
-
-        logger.debug("File list updated: ${fileList.joinToString()}")
     }
 
     // size1: 1920, size2: 1080, standardSize: 1600 => 900
