0b9638775fffd692bedf5e5ba0f402f3b379ce02
BlueGeckoJP <kei09doya@gmail.com>
2026-02-14 13:57:22 +0900

    chore: update Kotlin and JVM versions to 25 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.3.10"
     id("com.gradleup.shadow") version "9.2.2"
     application
 }
@@ -15,7 +15,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.10")
     // https://mvnrepository.com/artifact/com.twelvemonkeys.imageio/imageio-core
     implementation("com.twelvemonkeys.imageio:imageio-core:3.11.0")
     // https://mvnrepository.com/artifact/com.twelvemonkeys.imageio/imageio-webp
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.yaml:snakeyaml:2.3")
     implementation("info.picocli:picocli:4.7.6")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.17")
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
@@ -38,7 +38,7 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(22)
+    jvmToolchain(25)
 }
 
 tasks.withType<Jar> {
